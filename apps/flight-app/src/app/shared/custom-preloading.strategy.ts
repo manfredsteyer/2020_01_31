cfc140e012862cdab55a6d1fad70942d4eafea66
Manfred Steyer <manfred.steyer@gmx.net>
2020-01-31 09:04:36 +0100

    Tag 1

---
+++ apps/flight-app/src/app/shared/custom-preloading.strategy.ts
@@ -0,0 +1,20 @@
+
+
+import { Injectable } from '@angular/core';
+import { PreloadingStrategy, Route } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class CustomPreloadingStrategy implements PreloadingStrategy {
+    constructor() { }
+
+    
+    preload(route: Route, fn: () => Observable<any>): Observable<any> {
+        
+
+        return fn();
+
+    }
+
+
+}
